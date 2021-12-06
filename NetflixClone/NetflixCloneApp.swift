781e3ef3efb3fc4f94e029b12b12f2d3b9a68b06
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2021-12-05 23:21:12 -0500

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Juan Sebastian Orozco Buitrago on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
